78e3834fa4a4ae5ff87cf0c3ff84eff08abd8adc
hakohumi <mihumihu32@gmail.com>
2020-05-20 00:08:09 +0900

    追記

--- Grouping_photos/grouping_photos/source/grouping_photos.c
+++ Grouping_photos/grouping_photos/source/grouping_photos.c
@@ -1,15 +1,20 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
 
 #include "InputFile.h"
 
+// https://programming.pc-note.net/c/file7.html
 
-int main() {
+int main()
+{
     printf("group_photo start\n");
 
     // 写真ファイルの入ったフォルダの入力
     InputFile();
 
     // 出力ディレクトリへ写真ファイルのコピー
+
+    system("dir");
 
     // ファイルのリネーム
 
