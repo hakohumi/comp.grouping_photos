e96db1c4cbbe17676cae0100063dbd733b6cb961
Fuminori Hakoishi <fuminori.hakoishi@erii.co.jp>
2020-05-20 11:20:36 +0900

    ファイルリネーム機能 追加

--- Grouping_photos/grouping_photos/source/grouping_photos.c
+++ Grouping_photos/grouping_photos/source/grouping_photos.c
@@ -5,16 +5,16 @@
 
 // https://programming.pc-note.net/c/file7.html
 
-int main()
-{
+int main() {
     FileName i_fileName = "grouping_photos/resource/in_test.txt";
+    FileName o_fileName = "grouping_photos/resource/in_test2.txt";
     printf("group_photo start\n");
 
     // 1つの写真ファイルの入力
     InputFile(i_fileName);
 
     // 出力ディレクトリへ写真ファイルのコピー
-    CopyFile();
+    MyCopyFile(i_fileName, o_fileName);
 
     // ファイルのリネーム
 
